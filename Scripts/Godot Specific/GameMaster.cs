cd0820142904242a7b46bc716494e8580e4622e3
Henry Foster <henryfoster013@gmail.com>
2026-04-13 21:45:17 +0100

    Added UI that displays the current mode and turn. Also went ahead and added a cool gradient from the Unity version

--- Scripts/Godot Specific/GameMaster.cs
+++ Scripts/Godot Specific/GameMaster.cs
@@ -6,13 +6,18 @@ public partial class GameMaster : Node {
 	[Export] public string map_json_path = "res://Board/map_data.json";
 	[Export] public MapRenderer map_renderer;
 	[Export] public LabelManager label_manager;
-	
-	[Export] public ColorRect ui_player_colour;
+
+	[Export] public TextureRect ui_player_colour;
 	[Export] public Label ui_player_name;
+	[Export] public Label ui_game_state;
+	[Export] public Label ui_game_additional;
 
 	private GameManager manager;
 	public IReadOnlyDictionary<string, Territory> Territories => manager.Territories;
 	public IReadOnlyDictionary<string, Region> Regions => manager.Regions;
+
+	int turn_counter = 0;
+	Player current_player;
 
 	// Start //
 
@@ -34,7 +39,7 @@ public partial class GameMaster : Node {
 	public void SelectTerritory(Territory territory) {
 
 		map_renderer.SelectTerritory(territory);
-		
+
 		if (territory == null)
 			return;
 		if (manager.game_state == GameManager.state_type.CLAIMANTS) {
@@ -43,16 +48,47 @@ public partial class GameMaster : Node {
 	}
 
 	public void NewPlayerTurn(Player player) {
-		if (ui_player_name == null || ui_player_colour == null)
-			return;
-		if (player == null) {
-			ui_player_name.Text = "";
-			ui_player_colour.Color = new Color(0,0,0,0);
-			return;
-		}
-		ui_player_name.Text = player.name;
-		ui_player_colour.Color = player.colour;
+		current_player = player;
+		turn_counter++;
+		UpdateUI();
+
 	}
+
+	// UI Management //
+
+	void UpdateUI() {
+		UpdatePlayerLabels();
+		UpdateGameInfoLabels();
+	}
+
+	void UpdatePlayerLabels() {
+		if(ui_player_name == null || ui_player_colour == null)
+			return;
+		if (current_player == null) {
+			ui_player_name.Text = "";
+			ui_player_colour.Modulate = new Color(0, 0, 0, 0);
+		}
+		else {
+			ui_player_name.Text = current_player.name;
+			ui_player_colour.Modulate = current_player.colour;
+		}
+	}
+
+	void UpdateGameInfoLabels() {
+		if(ui_game_state == null || ui_game_additional == null)
+			return;
+		ui_game_state.Text = "";
+		ui_game_additional.Text = "";
+		switch (manager.game_state) {
+			case GameManager.state_type.NULL: ui_game_state.Text = "NULL"; break;
+			case GameManager.state_type.LOBBY: ui_game_state.Text = "Lobby"; break;
+			case GameManager.state_type.CLAIMANTS: ui_game_state.Text = "Claimants"; ui_game_additional.Text = TurnText(); break;
+			case GameManager.state_type.PRIMARY: ui_game_state.Text = "Primary"; ui_game_additional.Text = TurnText(); break;
+			case GameManager.state_type.ENDGAME: ui_game_state.Text = "Endgame"; break;
+		}
+	}
+
+	string TurnText() => $"Turn {turn_counter}";
 
 	// Scene Transitions //
 
